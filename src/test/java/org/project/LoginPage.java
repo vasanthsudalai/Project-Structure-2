11445698ad8f74756c74e570c356ce099b4ba354
vasanthsudalai <msvasanth.mech@gmail.com>
2023-02-08 18:25:08 +0530

    Vasanth's Code

--- src/test/java/org/project/LoginPage.java
+++ src/test/java/org/project/LoginPage.java
@@ -5,6 +5,10 @@ public static void main(String[] args) {
 	System.out.println("Test 4");
 	System.out.println("Test 5");
 	System.out.println("Test 6");
-
+    
+	System.out.println("Test 1");
+	System.out.println("Test 2");
+	System.out.println("Test 3");
+	
 }
 }
