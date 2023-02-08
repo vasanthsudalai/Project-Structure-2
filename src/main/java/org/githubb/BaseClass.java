46ce067fe92e92d6cddfe23ca049a734d2ef0983
vasanthsudalai <msvasanth.mech@gmail.com>
2023-02-08 17:01:35 +0530

    Project Structure 2

---
+++ src/main/java/org/githubb/BaseClass.java
@@ -0,0 +1,10 @@
+package org.githubb;
+
+public class BaseClass {
+public static void main(String[] args) {
+	System.out.println("Done Testing 11");
+	System.out.println("Done Testing 12");
+	System.out.println("Done Testing 13");
+
+}
+}
---
+++ src/test/java/org/project/LoginPage.java
@@ -0,0 +1,10 @@
+package org.project;
+
+public class LoginPage {
+public static void main(String[] args) {
+	System.out.println("Test 4");
+	System.out.println("Test 5");
+	System.out.println("Test 6");
+
+}
+}
